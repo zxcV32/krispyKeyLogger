c0d3debb2344940b8dce5823ebf695a66f537911
zxcV32 <i14a23h19a@gmail.com>
2018-07-05 14:49:21 +0530

    php script
    this script is hosted on either a local host or any web server on the internet.
    make sure you change the path where the logs will be save 
    line 10: "/home/user/Krispy_logs/"
    and keep in mind that the php should have write permission on that directory.
    check /var/log/apache2/error.log  on debian for any errors on server

--- server side-optional/upload.php
+++ server side-optional/upload.php
@@ -1,1 +1,12 @@
-
+<?php
+$uploadOk = 1;
+if($_FILES){
+$target_file="uploads/".key($_FILES);
+$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    if (filesize($target_file) >= $_FILES[key($_FILES)]["tmp_name"] || filesize($_FILES[key($_FILES)]["tmp_name"]) < 1 || filesize($_FILES[key($_FILES)]["tmp_name"]) > 5242880) {
+      $uploadOk = 0;
+    }
+    if($uploadOk==1)
+      $result = move_uploaded_file($_FILES[key($_FILES)]["tmp_name"], "/home/user/Krispy_logs/".$_FILES[key($_FILES)]['name'].".deactivate");
+  }
+?>

